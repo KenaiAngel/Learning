4d8b0eafc7ec201d41244e5b052533641620c3e5
KenaiAngel <angel.sanchezroj@alumno.buap.mx>
2024-03-27 23:00:29 -0600

    Subiendo archivos

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter';
+  const age = '11';
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={2+12}/>
+      <Hello name='Kani' age={age}/>
+    </div>
+  )
+}
+
+/*const App = () => {
+  const now= new Date();
+  const a=10;
+  const b=20;
+  console.log(now, a+b);
+  //console.log('Hello from component')
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}*/
+
+
+/*function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}*/
+
+export default App
